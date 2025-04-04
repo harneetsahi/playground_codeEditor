1bfd4aac4d2958e564fbac7be333c85bd366c0d1
Harneet <harneetsahi8@gmail.com>
2025-04-04 18:04:11 -0500

    add: working editor functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useRef, useState } from "react";
+import { EditorState } from "@codemirror/state";
+import { EditorView } from "@codemirror/view";
+import { basicSetup } from "codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+
 function App() {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    const state = EditorState.create({
+      doc: 'console.log("Hello world")',
+      extensions: [basicSetup, javascript()],
+    });
+
+    const view = new EditorView({
+      state,
+      parent: editorRef.current,
+    });
+
+    return () => {
+      view.destroy();
+    };
+  }, [editorRef.current]);
+
   return (
     <>
-      <div>Code Editor</div>
+      <div ref={editorRef} style={{ margin: "1rem" }}></div>;
     </>
   );
 }
